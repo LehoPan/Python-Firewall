40ae8da7943adde5e7da99eaa55da8bbb90b7903
Leo Pan <leo.long.pan@gmail.com>
2024-11-17 22:22:07 +0000

    added basic ping defense firewall

--- pingattack.py
+++ pingattack.py
@@ -1,12 +1,13 @@
 import subprocess
 import time
 
-targetIP = XYZ  #targetIP IP, found by going into cmd and typing 'ipconfig'
+targetIP = '192.168.2.2'  #targetIP IP, found by going into cmd and typing 'ipconfig'
 
 print(f"Starting ping attack on {targetIP}.")
 try:
     while True:     #infinite loop of pinging until stopped
-        if subprocess.call(["ping", "-n", "1", targetIP]) == 0: #sends single ping to target IP address
+        #give -c as the second arg if the attacker host is linux, -n if it is windows
+        if subprocess.call(["ping", "-c", "1", targetIP]) == 0: #sends single ping to target IP address,
             print(f"Ping to {targetIP} successful.")            #confirmation if ping is successful
         else:
             print(f"Ping to {targetIP} blocked/failed.")        #confirmation if ping failed or gets blocked
